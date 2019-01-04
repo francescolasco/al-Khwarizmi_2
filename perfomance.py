852c38c238aed78b76bc706bf13407c659538685
Giorbo <jekmindfreak8@gmail.com>
2019-01-04 18:41:51 +0100

    c

--- perfomance.py
+++ perfomance.py
@@ -2,19 +2,19 @@ from Graph_IncidenceList_MOD import *
 from random import randint
 
 def generateWeightedGraph(numNodes):
-	graph = GraphIncidenceList()
-	nodes = []
+    graph = GraphIncidenceList()
+    nodes = []
 
-	graph.print()
+    graph.print()
 
-	for i in range(numNodes):
-		node = graph.addNode(i, randint(1, numNodes))
-		nodes.append(node)
+    for i in range(numNodes):
+        node = graph.addNode(i, randint(1, numNodes))
+        nodes.append(node)
 
-	for node_src in nodes:
-		for node_dst in nodes:
-			if node_src != node_dst:
-				print("---")
+    for node_src in nodes:
+        for node_dst in nodes:
+            if node_src != node_dst:
+                print("---")
                 print("Adjacent nodes {},{}: {}".format(node_src.id, node_dst.id, graph.isAdj(node_src.id, node_dst.id)))
                 graph.insertEdge(node_src.id, node_dst.id, node_src.id + node_dst.id)
                 print("Edge inserted: from {} to {}".format(node_src.id, node_dst.id))
@@ -23,4 +23,4 @@ def generateWeightedGraph(numNodes):
                 print("---")
     print("Num Nodes:", graph.numNodes())
     print("Num Edges:", graph.numEdges())
-	graph.print()
+    graph.print()
