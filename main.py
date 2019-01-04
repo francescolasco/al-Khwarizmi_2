222aef0fad3047b51ffcde582e7f5bd7245729a3
Giorbo <jekmindfreak8@gmail.com>
2019-01-04 16:17:01 +0100

    modified main.py

--- main.py
+++ main.py
@@ -3,25 +3,31 @@ from PQbinaryHeap import PQbinaryHeap
 from PQbinomialHeap import PQbinomialHeap
 from graph.Graph import *
 
-def prioritySearch(graph, pqType):
+def prioritySearch_PQbinaryHeap(graph):
 	"""
 	This algorithm executes a prioritySearch based on the genericSearch of a graph. 
 	It uses a list to represent the visit of the graph and a priority queue for the nodes to explore.
 	:param graph: a graph to explore.
 	:return: the list of the exploration.
 	"""
-	if pqType == 'PQbinary':
-        pq = PQbinaryHeap()
-        print("Using", pqType)
-    elif pqType == 'PQbinomial':
-        pq = PQbinomialHeap()
-        print("Using", pqType)
-    elif pqType == 'PQ_Dheap' and d is not None:  #'d' must be set.
-        pq = PQ_DHeap(d)
-        print("Using ", pqType, "with d = ", d)
-    else:
-        return
-
+	pq = PQbinaryHeap()
 	exploredNodes = []
 
+	s = graph.maxWeight()
+	ID = s[0]
+	KEY = -(s[1])
+	pq.insert(ID, KEY)
+
 	while not pq.isEmpty():
+		u = pq.findMin()
+		pq.deleteMin()
+
+		exploredNodes.append(u)
+		
+		for vertex in graph.getAdj(u):
+			ID = vertex.id
+			KEY = -(vertex.key)
+			if ID not in exploredNodes:
+				pq.insert(ID, KEY)
+
+	return exploredNodes
