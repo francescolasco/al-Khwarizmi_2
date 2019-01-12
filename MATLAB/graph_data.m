208775f951bd534a84fbcba34b2e65a323a263e0
Francesco <francescolasco@gmail.com>
2019-01-12 16:54:36 +0100

    MATLAB analysis

---
+++ MATLAB/graph_data.m
@@ -0,0 +1,33 @@
+ 
+%prepare binary heap data:
+binaryHeap = fullfile('performanceData','performancebinaryHeap.txt');
+binaryID = fopen(binaryHeap);
+binaryScan = textscan(binaryID, '%d %f');
+
+%prepare d-heap data:
+dHeap = fullfile('performanceData','performancedHeap.txt');
+dID = fopen(dHeap);
+dScan = textscan(dID, '%d %f');
+
+%prepare binomial heap data:
+binomialHeap = fullfile('performanceData', 'performancebinomialHeap.txt');
+binomialID = fopen(binomialHeap);
+binomialScan = textscan(binomialID, '%d %f');
+
+%close files:
+fclose(binaryID);
+fclose(binomialID);
+fclose(dID);
+
+%create new figure
+figure
+hold on;
+legend();
+plot(binaryScan{1},binaryScan{2},'DisplayName', 'Binary Heap')
+plot(binomialScan{1},binomialScan{2},'DisplayName','Binomial Heap')
+plot(dScan{1},dScan{2},'DisplayName','D-Heap')
+title('Priority Queue Implementation Comparison:');
+xlabel('Number of Vertices.');
+ylabel('Time in seconds.')
+pbaspect([2 1 1]);
+hold off;
