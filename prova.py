d32362f09d7653125f24503bf282ef32329d54b6
Giorbo <jekmindfreak8@gmail.com>
2019-01-13 16:38:45 +0100

    fixed bug on Graph class

--- prova.py
+++
@@ -1,9 +0,0 @@
-from performance import *
-
-def disegnaGrafo(nVertices):
-    g = buildGraph(nVertices)
-    addEdges(g,1)
-    print(g.numEdges())
-    for i in g.getEdges():
-        print("Da = "+str(i.head))
-        print("A = "+str(i.tail))
