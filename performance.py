e54b41dcdc4fdbd82f8fc726b288be38da793822
Giorbo <jekmindfreak8@gmail.com>
2019-01-13 19:36:44 +0100

    finished data for graphs

--- performance.py
+++ performance.py
@@ -5,70 +5,79 @@ from timer import timer
 from time import *
 
 
-################################ V1 ###################################
-def performanceStrana(nVertices, d = None):
+def performanceGraphIL(graph, version, d = None):
     assert d != None, "You must provide a 'd' for the D-Heap test!"
-    graph = buildGraph(nVertices)
+    binaryHeapVertices = open("performanceData/performancebinaryHeapVertices.txt", "a")
+    binaryHeapEdges = open("performanceData/performancebinaryHeapEdges.txt", "a")
+    binomialHeapVertices = open("performanceData/performancebinomialHeapVertices.txt", "a")
+    binomialHeapEdges = open("performanceData/performancebinomialHeapEdges.txt", "a")
+    dHeapVertices = open("performanceData/performancedHeapVertices.txt", "a")
+    dHeapEdges = open("performanceData/performancedHeapEdges.txt", "a")
 
-    @timer
-    def testPrioritySearch_PQbinaryHeap():
-        graph.prioritySearch_PQbinaryHeap()
-
-    @timer
-    def testPrioritySearch_PQbinomialHeap():
-        graph.prioritySearch_PQbinomialHeap()
-
-    @timer
-    def testPrioritySearch_PQ_DHeap(d):
-        graph.prioritySearch_PQ_DHeap(d)
-
-    testPrioritySearch_PQbinaryHeap()
-    testPrioritySearch_PQbinomialHeap()
-    testPrioritySearch_PQ_DHeap(d)
-
-################################# V2 ##################################
-
-
-
-def performanceGraphIL(nVertices, d = None):
-    assert d != None, "You must provide a 'd' for the D-Heap test!"
-    binaryHeap = open("performanceData/performancebinaryHeap.txt", "a")
-    binomialHeap = open("performanceData/performancebinomialHeap.txt", "a")
-    dHeap = open("performanceData/performancedHeap.txt", "a")
-    graph = buildGraph(nVertices)
+    nVertices = len(graph.getNodes())
+    nEdges = graph.numEdges()
 
     start = time()
     graph.prioritySearch_PQbinaryHeap()
     tempo = time()-start
-    binaryHeap.write("{}\t{}\n".format(nVertices, tempo))
+    if version == 0:
+        binaryHeapVertices.write("{}\t{}\n".format(nVertices, tempo))
+    elif version == 1:
+        binaryHeapEdges.write("{}\t{}\n".format(nEdges, tempo))
 
 
     start = time()
     graph.prioritySearch_PQbinomialHeap()
     tempo = time()-start
-    binomialHeap.write("{}\t{}\n".format(nVertices, tempo))
+    if version == 0:
+        binomialHeapVertices.write("{}\t{}\n".format(nVertices, tempo))
+    elif version == 1:
+        binomialHeapEdges.write("{}\t{}\n".format(nEdges, tempo))
 
 
     start = time()
     graph.prioritySearch_PQ_DHeap(d)
     tempo = time()-start
-    dHeap.write("{}\t{}\n".format(nVertices, tempo))
+    if version == 0:
+        dHeapVertices.write("{}\t{}\n".format(nVertices, tempo))
+    elif version == 1:
+        dHeapEdges.write("{}\t{}\n".format(nEdges, tempo))
 
-    binaryHeap.close()
-    binomialHeap.close()
-    dHeap.close()
+    binaryHeapVertices.close()
+    binaryHeapEdges.close()
+    binomialHeapVertices.close()
+    binomialHeapEdges.close()
+    dHeapVertices.close()
+    dHeapEdges.close()
 
 
-numberOfVertices = [10, 100, 1000, 5000, 10000]
-def dataForGraphs(nVertices, d):
-    for i in nVertices:
-        performanceGraphIL(i, d)
 
-def dataForGraphs_(maxV, d):
-    for i in range(10,maxV+1,10):
-        performanceGraphIL(i, d)
+
+def dataVertices(maxV, d):
+    for vertices in range(10,maxV+1,10):
+        graph = buildGraph(vertices)
+        performanceGraphIL(graph, 0, d)
+
+
+def dataEdges(nVertices, d):
+    graph = buildGraph(nVertices)
+    nEdges = graph.numEdges()
+    maxEdges = int((nVertices*(nVertices-1)))-nEdges
+    print("maxE = "+str(maxEdges))
+    for edges in range(100):
+        performanceGraphIL(graph, 1, d)
+        addEdges(graph, int(maxEdges/100))
+
+
+def dataForGraphs(nVertices, maxV, d):
+    dataVertices(maxV, d)
+    dataEdges(nVertices, d)
+
 
 def ereaseDataFromGraphs():
-    binaryHeap = open("performanceData/performancebinaryHeap.txt", "w+").close()
-    binomialHeap = open("performanceData/performancebinomialHeap.txt", "w+").close()
-    dHeap = open("performanceData/performancedHeap.txt", "w+").close()
+    binaryHeapVertices = open("performanceData/performancebinaryHeapVertices.txt", "w+").close()
+    binaryHeapEdges = open("performanceData/performancebinaryHeapEdges.txt", "w+").close()
+    binomialHeapVertices = open("performanceData/performancebinomialHeapVertices.txt", "w+").close()
+    binomialHeapEdges = open("performanceData/performancebinomialHeapEdges.txt", "w+").close()
+    dHeapVertices = open("performanceData/performancedHeapVertices.txt", "w+").close()
+    dHeapEdges = open("performanceData/performancedHeapEdges.txt", "w+").close()
